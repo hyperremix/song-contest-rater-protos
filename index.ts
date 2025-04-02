668f795eee2aae4b6d130952a2e81644652f925e
Fredrik Pettersson <fredrik.pettersson.89@gmail.com>
2025-04-02 11:58:31 +0200

    Fix package build

---
+++ index.ts
@@ -0,0 +1,42 @@
+export {
+  ActResponse,
+  CreateActRequest,
+  DeleteActRequest,
+  GetActRequest,
+  ListActsResponse,
+  UpdateActRequest,
+} from "./act";
+export {
+  CompetitionResponse,
+  CreateCompetitionRequest,
+  DeleteCompetitionRequest,
+  GetCompetitionRequest,
+  ListCompetitionsResponse,
+  UpdateCompetitionRequest,
+} from "./competition";
+export {
+  CreateParticipationRequest,
+  ListParticipationsResponse,
+  ParticipationResponse,
+} from "./participation";
+export {
+  CreateRatingRequest,
+  DeleteRatingRequest,
+  GetRatingRequest,
+  ListRatingsResponse,
+  RatingResponse,
+  UpdateRatingRequest,
+} from "./rating";
+export {
+  GlobalStatsResponse,
+  ListUserStatsResponse,
+  UserStatsResponse,
+} from "./stat";
+export {
+  CreateUserRequest,
+  DeleteUserRequest,
+  GetUserRequest,
+  ListUsersResponse,
+  UpdateUserRequest,
+  UserResponse,
+} from "./user";
